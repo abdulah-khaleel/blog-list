b319c582ca5d172a1a91b6fc18fd312c9669fed2
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-29 12:46:47 +0500

    [Add] 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,5 +1,6 @@
 const listHelper = require('../utils/list_helper')
 const totalLikes = require('../utils/list_helper').totalLikes
+const favoriteBlog = require('../utils/list_helper').favoriteBlog
 
 test('dummy returns one', () => {
   const blogs = []
@@ -80,5 +81,115 @@ describe('total likes', () => {
     ]
 
     expect(totalLikes(blogs)).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    expect(favoriteBlog(blogs)).toBe('empty list')
+  })
+
+  test('when a list has only one blog with no likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 0,
+        __v: 0,
+      },
+    ]
+    expect(favoriteBlog(listWithOneBlog)).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 0,
+    })
+  })
+
+  test('when a list has only one blog with some likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 8,
+        __v: 0,
+      },
+    ]
+    expect(favoriteBlog(listWithOneBlog)).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 8,
+    })
+  })
+
+  test('total likes of a bigger list that is calculated right', () => {
+    const blogs = [
+      {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0,
+      },
+      {
+        _id: '5a422b891b54a676234d17fa',
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+        __v: 0,
+      },
+      {
+        _id: '5a422b3a1b54a676233rw7f9',
+        title: 'Demo Book',
+        author: 'Demo Author',
+        url: 'http://www.cs.utexas.edu/~EWD/translations/3201.html',
+        likes: 12,
+        __v: 0,
+      },
+      {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+        __v: 0,
+      },
+      {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0,
+      },
+    ]
+
+    expect(favoriteBlog(blogs)).toEqual({
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+    })
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((a, b) => +a + +b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length > 0) {
+    const favBlog = blogs.find(
+      (blog) => blog.likes === Math.max(...blogs.map((o) => o.likes))
+    )
+    return {
+      title: favBlog.title,
+      author: favBlog.author,
+      likes: favBlog.likes,
+    }
+  }
+  return 'empty list'
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
